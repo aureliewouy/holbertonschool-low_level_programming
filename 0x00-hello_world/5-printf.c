6518aebd0cb79a88e952126cf45548039495957b
aureliewouy <aureliecedia@yahoo.com>
2020-09-16 11:05:18 +0000

    changes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,6 @@
-#!/bin/bash
-printf ("with proper grammar, but the outcome is a piece of art,\n");
-
+#include <stdio.h>
+#include <string.h>
+int main (void) {
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
