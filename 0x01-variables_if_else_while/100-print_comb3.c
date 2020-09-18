6cba2944adcb1869b1bc4af2d355e2cc080a4e8d
aureliewouy <aureliecedia@yahoo.com>
2020-09-18 08:36:30 +0000

    changes

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-  long int n, n1, n2;
+long int n, n1, n2;
 for (n = 0; n < 100; n++)
 {
-  n1 = ((n / 10) + '0');
-  n2 = ((n % 10) + '0');
-  if (n1 != n2 && n1 < n2 )
-{
-  putchar((n / 10) + '0');
-  putchar((n % 10) + '0');
+n1 = ((n / 10) + '0');
+n2 = ((n % 10) + '0');
+if (n1 != n2 && n1 < n2)
+{y
+putchar((n / 10) + '0');
+putchar((n % 10) + '0');
 if (n != 89)
 {
 putchar(',');
