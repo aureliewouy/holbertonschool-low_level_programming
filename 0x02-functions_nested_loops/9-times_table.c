2878368a44b92e450af7e2485eb15775c72bd76e
aureliewouy <aureliecedia@yahoo.com>
2020-09-21 15:37:44 +0000

    changes

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * times_table - Print the alphabet.
+ *
+ * Return: Always 0.
+ */
+
+void times_table(void)
+{
+	long int n = 0;
+	long int compteur = 0;
+	long int nn = 0;
+	long int resultat;
+	while(nn < 10)
+	{
+		while (compteur < 10)
+		{
+			resultat = n * compteur;
+			if (resultat < 10)
+			{
+				_putchar((resultat) + '0');
+			}
+			else
+			{
+				_putchar((resultat / 10) + '0');
+				_putchar((resultat % 10) + '0');
+			}
+			_putchar(',');
+			compteur++;
+		}
+	_putchar('\n');
+	nn++;
+	n++;
+	compteur = 0;
+	}
+}
