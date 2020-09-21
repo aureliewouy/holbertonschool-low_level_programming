b8e9dec461460d7339e6a7bfc8c5fa283ef751b5
aureliewouy <aureliecedia@yahoo.com>
2020-09-21 13:39:25 +0000

    changes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * _isalpha - check the code for Holberton School students.
+ * print_sign - check the code for Holberton School students.
  *@n: is the letter that we test.
  *
  * Return: Always 0.
