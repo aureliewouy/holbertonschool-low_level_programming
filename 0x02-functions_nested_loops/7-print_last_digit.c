29f633e6bad5df849a6fb80ab67d9693020cbb37
aureliewouy <aureliecedia@yahoo.com>
2020-09-22 21:01:25 +0000

    changes

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,11 @@ int print_last_digit(int n)
 {
 	int lastDigit;
 
+	lastDigit = n % 10;
 	if (lastDigit < 0)
 	{
-		n = -n;
+		lastDigit = -lastDigit;
 	}
-	lastDigit = n % 10;
 	_putchar((lastDigit) + '0');
 
 	return (lastDigit);
