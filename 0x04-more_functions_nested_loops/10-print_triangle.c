da2e5f816c46d57aead7913daf9aa5b2bcea0236
aureliewouy <aureliecedia@yahoo.com>
2020-09-24 21:11:00 +0000

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,21 +12,21 @@ void print_triangle(int size)
 {
 	int nb1 = 1, nb2 = size, nb3 = 1;
 
-	if ( size <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
-	while ( nb1 <= size)
+	while (nb1 <= size)
 	{
 		nb1++;
 		if (size > 1)
 		{
-			while(nb2 >= nb1)
+			while (nb2 >= nb1)
 			{
 				_putchar(' ');
 				nb2--;
 			}
-			while(nb3 < nb1)
+			while (nb3 < nb1)
 			{
 				_putchar('#');
 				nb3++;
