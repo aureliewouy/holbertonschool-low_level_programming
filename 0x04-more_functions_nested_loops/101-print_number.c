7bca01c992056481bf02ea1f64d49f8a91e6f0e5
aureliewouy <aureliecedia@yahoo.com>
2020-09-24 21:58:38 +0000

    changes

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * print_number - Print the alphabet.
+ * @n: this is the number to print
+ *
+ * Return: Always 0.
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+		_putchar(n + '0');
+		/*   _putchar((n % 10) + '0');*/
+	}
+	if (n < 9)
+	{
+		_putchar(n + '0');
+	}
+	else if (n < 99)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n < 999)
+	{
+		_putchar((n / 100) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n < 9999)
+	{
+		_putchar((n / 1000) + '0');
+		_putchar(((n / 100) % 10) + '0');
+                _putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+}
