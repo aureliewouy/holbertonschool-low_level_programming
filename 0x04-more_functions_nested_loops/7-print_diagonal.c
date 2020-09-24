15f4c358300badfe196b6b998c8192887c119754
aureliewouy <aureliecedia@yahoo.com>
2020-09-24 19:43:15 +0000

    diag

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - check the code for Holberton School students.
+ * @n: the number of line to print
+ *
+ * Return: Always 0.
+ */
+
+void print_diagonal(int n)
+{
+	char line = 92;
+	char esp = ' ';
+	int i = 0, j = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	while (i < n)
+	{
+		i++;
+		while (j < i)
+		{
+			_putchar(esp);
+			j++;
+		}
+		_putchar(line);
+		_putchar('\n');
+		j = 0;
+	}
+}
