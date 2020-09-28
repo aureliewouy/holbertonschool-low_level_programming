02f21a2cb791feeab0b6d02249dc2f57a48735b7
aureliewouy <aureliecedia@yahoo.com>
2020-09-28 20:53:49 +0000

    first commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -13,8 +13,8 @@ void _puts(char *str)
 
 	while (str[n])
 	{
-		putchar(str[n]);
+		_putchar(str[n]);
 		n++;
 	}
-	putchar('\n');
+	_putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_rev - print a string in reverse
+ * @s : the string that we will print in reverse
+ *
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int n = 0;
+
+	while (s[n])
+	{
+		n++;
+	}
+	while (s[n] >= 0)
+	{
+		_putchar(s[n]);
+		n--;
+	}
+	_putchar('\n');
+}
