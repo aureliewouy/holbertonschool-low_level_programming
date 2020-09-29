1f8e1429b73a58829eaf9dae84e7b2e3f121958d
aureliewouy <aureliecedia@yahoo.com>
2020-09-29 21:27:27 +0000

    changes

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,13 +14,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if ((i + 1) == n)
+		printf("%d", a[i]);
+		if (i < n - 1)
 		{
-			printf("%d\n", a[i]);
-		}
-		else
-		{
-			printf("%d, ", a[i]);
+			printf(", ");
 		}
 	}
+	printf("\n");
 }
