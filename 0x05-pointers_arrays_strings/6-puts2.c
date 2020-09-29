76b6a69ead837f223d52fef99d2ba9041d8dc1dc
aureliewouy <aureliecedia@yahoo.com>
2020-09-29 21:56:40 +0000

    changes

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,25 @@
 #include "holberton.h"
 
 /**
+ * _strlen - returns the lenght of a string
+ * @s : the string that we will find the length
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+        int n = 0;
+
+        while (s[n])
+        {
+                ++n;
+        }
+        return (n);
+}
+
+
+/**
  * puts2 - print a string
  * @str : the string that we will print
  *
@@ -9,9 +28,9 @@
 
 void puts2(char *str)
 {
-	int n = 0;
+	int n = 0, len = _strlen(str);
 
-	while (str[n])
+	while (n < len)
 	{
 		_putchar(str[n]);
 		n += 2;
