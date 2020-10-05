5fb87228f64cf23a5d33c2cdcfba3cb4d0c6d718
aureliewouy <aureliecedia@yahoo.com>
2020-10-05 13:15:35 +0000

    first commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * *_strchr - Locates a character in a string
+ * @s: the string
+ * @c: the character
+ *
+ * Return: the character of null if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *p = s;
+	int i = 0;
+
+	while (*s != c)
+	{
+		++s;
+		++i;
+	}
+	if (*s == c)
+	{
+		return (p + i);
+	}
+	else
+	{
+		return (0);
+	}
+}
