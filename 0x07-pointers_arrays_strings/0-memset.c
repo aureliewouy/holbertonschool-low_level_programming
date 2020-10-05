3412e91b7678ee6af4a4b6d156c8d9156b46a22b
aureliewouy <aureliecedia@yahoo.com>
2020-10-05 15:20:37 +0000

    clean

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,7 +12,6 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-/*	char *str = s;*/
 
 	for (i = 0; i < n; i++)
 		s[i] = b;
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,10 +12,8 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	char *src_char = src;
-	char *dest_char = dest;
 
 	for (i = 0; i < n; i++)
-		dest_char[i] = src_char[i];
-	return (dest_char);
+		dest[i] = src[i];
+	return (dest);
 }
