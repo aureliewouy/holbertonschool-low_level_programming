a88d23159a4936b8ee0c4b57da66690cbb3eabc9
aureliewouy <aureliecedia@yahoo.com>
2020-10-07 15:26:16 +0000

    fix

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,12 +9,12 @@
 
 int _prime(int n, int i)
 {
-	if(n % i == 0)
+	if (n % i == 0)
 	{
 		return (0);
 	}
 
-	if (n == i)
+	if (i ==  n - 1)
 	{
 		return (1);
 	}
@@ -30,9 +30,7 @@ int _prime(int n, int i)
 
 int is_prime_number(int n)
 {
-	if (n == 1)
-		return(0);
-	if (n < 0)
+	if (n <= 1)
 		return (0);
 	return (_prime(n, 2));
 }
