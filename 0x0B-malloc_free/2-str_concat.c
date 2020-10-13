4edb213b39daf030ad4c95ee9145694764db6376
aureliewouy <aureliecedia@yahoo.com>
2020-10-13 15:08:23 +0000

    fix checker

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - lenght of a string
+ * @s: the string
+ *
+ * Return: lenght
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
  * *str_concat - concatenates two strings.
  * @s1: the first string
  * @s2: the second string
@@ -12,31 +28,29 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s3;
-	char *s;
+	int len1, len2;
 	int i = 0, j = 0;
 
 	if (s1 == NULL)
 		*s1 = '\0';
 	if (s2 == NULL)
 		*s2 = '\0';
-	while (s1[i])
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	s3 = malloc(len1 + len2 + 1 * sizeof(char));
+	if (s3 == NULL)
+		return (NULL);
+	while (i < len1)
+	{
+		s3[i] = s1[i];
 		i++;
-	while (s2[j])
+	}
+	while (j < len2)
+	{
+		s3[i] += s2[j];
 		j++;
-	s3 = malloc((i + j) + 1 * sizeof(char));
-	s = s3;
-	while (*s1)
-	{
-		*s = *s1;
-		s++;
-		s1++;
+		i++;
 	}
-	while (*s2)
-	{
-		*s += *s2;
-		s++;
-		s2++;
-	}
-	s = '\0';
+	s3[i] = '\0';
 	return (s3);
 }
