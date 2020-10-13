934e133ee11807a43a26faf6f926c4bf2167412a
aureliewouy <aureliecedia@yahoo.com>
2020-10-13 13:53:31 +0000

    add 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *str_concat - concatenates two strings.
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: newly allocated space in memory.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *s3;
+	char *s;
+	int i = 0, j = 0;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		*s1 = ' ';
+		*s2 = ' ';
+	}
+	while(s1[i])
+		i++;
+        while(s2[j])
+		j++;
+	s3 = malloc((i + j) + 1 * sizeof(char));
+	s = s3;
+	while (*s1)
+	{
+		*s = *s1;
+		s++;
+		s1++;
+	}
+	  while (*s2)
+	{
+                *s += *s2;
+                s++;
+                s2++;
+		}
+	s = '\0';
+	return (s3);
+}
