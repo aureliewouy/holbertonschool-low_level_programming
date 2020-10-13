f6d8d444ebfb632bce79299fb335ec0c6d7a35c7
aureliewouy <aureliecedia@yahoo.com>
2020-10-13 10:48:42 +0000

    first commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *create_array - Creates an array of chars
+ * @size: the size of the array
+ * @c: the charater
+ *
+ * Return: a pointer to the array ofNULL if size = 0.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	s = malloc((size + 1) * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * *_strdup - a pointer to a newly allocated space in memory
+ * which contains a copy of the string given as a parameter.
+ * @str: the string.
+ *
+ * Return: a pointer to a new string or NULL.
+ */
+
+char *_strdup(char *str)
+{
+	char *new_s;
+	char *dup;
+	int size;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[size])
+		size++;
+	printf("size : %d", size);
+	new_s = malloc(size + 1);
+	dup = new_s;
+	while (*str)
+	{
+		*dup = *str;
+		dup++;
+		str++;
+	}
+	dup = '\0';
+	return (new_s);
+}
