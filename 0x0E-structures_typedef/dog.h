b7dfe431af2d50200ca8e50e67abd9dea4c34e80
aureliewouy <aureliecedia@yahoo.com>
2020-10-19 14:23:48 +0000

    first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Short description
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+ *
+ * Description: Longer description
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
