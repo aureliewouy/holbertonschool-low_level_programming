781a8e3ebbcbcdcf870f10f70b7f5db7553f1819
aureliewouy <aureliecedia@yahoo.com>
2020-10-25 08:50:56 +0000

    fix betty

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,10 +7,18 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+/**
+ * struct Format - find the format
+ * @c: the character
+ * @f: the function
+ *
+ * Description: with the format finds the right function for it
+ */
 typedef struct Format
 {
-        char c;
-        void (*f)(va_list);
+	char c;
+	void (*f)(va_list);
 } Format;
 void print_all(const char * const format, ...);
 #endif
