5f0c48a2aff16cafff6c6dd10a71e954e2310b53
aureliewouy <aureliecedia@yahoo.com>
2020-10-27 19:13:44 +0000

    fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -62,7 +62,7 @@ void print_s(va_list list)
 void print_all(const char * const format, ...)
 {
 	va_list valist;
-	int i = 0, j;
+	int i = 0, j, k = 0;
 	Format formats[] = {
 		{'c', print_c},
 		{'i', print_i},
@@ -80,9 +80,10 @@ void print_all(const char * const format, ...)
 		{
 			if (format[i] == formats[j].c)
 			{
-				if (i > 0)
+				if (k > 0)
 					printf(", ");
 				formats[j].f(valist);
+				k++;
 			}
 			j++;
 		}
