b953d172bee871edb9edc47a58af52fb2b626359
aureliewouy <aureliecedia@yahoo.com>
2020-11-04 11:09:41 +0000

    verify null

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,6 +28,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		while (count < index - 1)
 		{
+			if (temp == NULL)
+				return (-1);
 			temp = temp->next;
 			count++;
 		}
