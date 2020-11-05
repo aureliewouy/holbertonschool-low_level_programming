86d9aa3eb75fd15697f430ec4363b761ff52ef14
aureliewouy <aureliecedia@yahoo.com>
2020-11-05 17:50:58 +0000

    first commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: pointing to a string of 0 and 1 chars
+ *
+ * Return: the converted number of 0.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int k = 1;
+	unsigned int i = 0;
+	int c;
+	unsigned int len = 0;
+
+	while (b[len])
+	{
+		len++;
+	}
+	for (c = len - 1; c >= 0; c--)
+	{
+		if (b[c] != '0' && b[c] != '1')
+			return (0);
+		if (b[c] == '1')
+		{
+			i += k;
+		}
+		k *= 2;
+	}
+	return (i);
+}
