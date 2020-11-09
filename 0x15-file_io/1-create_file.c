f113f2a974bfae8fb30fee9355730353e6fc8fef
aureliewouy <aureliecedia@yahoo.com>
2020-11-09 18:25:39 +0000

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,9 +12,9 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, len = 0;
 
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1 || filename == NULL)
 		return (-1);
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	while (text_content[len])
 	{
 		len++;
