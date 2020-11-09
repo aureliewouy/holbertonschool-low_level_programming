49cb3469839587f78569d17643d1618f8f6b0c5b
aureliewouy <aureliecedia@yahoo.com>
2020-11-09 18:23:19 +0000

    fix write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len = 0;
+	int fd, wr, len = 0;
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
@@ -20,12 +20,12 @@ int create_file(const char *filename, char *text_content)
 	{
 		len++;
 	}
-	if (text_content != NULL)
-	{
-		write(fd, text_content, len);
-		read(fd, text_content, len);
-	}
-	close(fd);
+	if (text_content == NULL)
+		return (1);
 
+	wr = write(fd, text_content, len);
+	if (wr == -1)
+		return (-1);
+	close(fd);
 	return (1);
 }
