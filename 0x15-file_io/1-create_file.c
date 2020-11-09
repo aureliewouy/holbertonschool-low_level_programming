1ed40e57c0643ed219ca6f9f78aa9760382c849c
aureliewouy <aureliecedia@yahoo.com>
2020-11-09 17:55:42 +0000

    fix null

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,8 +20,11 @@ int create_file(const char *filename, char *text_content)
 	{
 		len++;
 	}
-	write(fd, text_content, len);
-	read(fd, text_content, len);
+	if (text_content != NULL)
+	{
+		write(fd, text_content, len);
+		read(fd, text_content, len);
+	}
 	close(fd);
 
 	return (1);
