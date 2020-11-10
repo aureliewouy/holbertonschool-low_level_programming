322adc370663c82814e40e4b1dbddade85c8eee8
aureliewouy <aureliecedia@yahoo.com>
2020-11-10 08:46:42 +0000

    add 3é

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include "holberton.h"
+
+/**
+ * main - Copies the content of a file to another file
+ *@argc: the size of argv array
+ *@argv: an array of size argc
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	char buf[1024];
+	int file_from, file_to, len, wr;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_CREAT, 0664);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		exit(99);
+	}
+	len = read(file_from, buf, 1024);
+	do	{
+		if (len == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		wr = write(file_to, buf, len);
+		if (wr == -1 || wr != len)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	} while (len == 1024);
+	if (close(file_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+	if (close(file_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+
+	return (0);
+}
