b743682ad1c4fff53a17574621951c5f9a69f8c1
aureliewouy <aureliecedia@yahoo.com>
2020-12-14 00:29:59 +0000

    fi index

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,14 +8,14 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i;
+	size_t i = 0;
+	const dlistint_t *tmp;
 
-	if (h == NULL)
-		return (0);
-	while (h != NULL)
+	tmp = h;
+	while (tmp != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
 		i++;
 	}
 	return (i);
