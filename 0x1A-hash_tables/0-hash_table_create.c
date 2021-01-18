a41370917d2d7aba9db008c381d830ea289a5f57
aureliewouy <aureliecedia@yahoo.com>
2021-01-18 08:24:24 +0000

    fix malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,14 +12,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 	unsigned long int i;
 
-	if (size < 1)
+	if (size == 0)
 		return (NULL);
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = (hash_node_t **)malloc(size * sizeof(hash_node_t));
+	table->array = malloc(size * sizeof(hash_node_t *));
 	if (table->array == NULL)
 		return (NULL);
 	for (i = 0; i < table->size; i++)
